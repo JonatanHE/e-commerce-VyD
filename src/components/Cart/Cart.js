cf6667501f5ac5ed4590cc6d1c5fdd2b8e682aec
Jonatan Sequeira <sequeirajonatanhe@gmail.com>
2022-08-25 23:02:32 -0300

    Desafío: Item Collection II

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,17 +1,132 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { CartContext } from "../../context/CartContext"
-import Checkout from "../../pages/Checkout"
+import './Cart.scss' 
+import Modal from '../Modal/Modal'
+import db from '../../firebaseConfig.js'
+import { collection, addDoc } from 'firebase/firestore'
 
 const Cart = () => {
-    const { cartProducts } = useContext(CartContext)
+    const [showModal, setShowModal] = useState(false)
+    const { cartProducts, totalPrice } = useContext(CartContext)
+    const [success, setSuccess] = useState()
 
+    const [order, setOrder] = useState({
+        items: cartProducts.map((product) => {
+            return {
+                id: product.id,
+                title: product.title,
+                price: product.price
+            }
+        } ),
+        buyer: {},
+        date: new Date().toLocaleString(),
+        total: totalPrice
+    })
+    const [formData, setFormData] = useState({
+        name: '',
+        phone: '',
+        email:''
+    })
+
+
+
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name] : e.target.value})
+    }
+
+    const submitData = (e) => {
+        e.preventDefault()
+        console.log("order para enviar: ", {...order, buyer: formData})
+        pushData({...order, buyer: formData})
+    }
+
+    const pushData = async (newOrder) => {
+        const collectionOrder = collection(db, 'ordenes')
+        const orderDoc = await addDoc(collectionOrder, newOrder)
+        setSuccess(orderDoc.id)
+        console.log('ORDEN GENERADA', orderDoc)
+    }
 
     return(
-        <>
-        <div>Carrito de checkout</div>
-        {console.log("cartProducts dede checkout: ",cartProducts)}
-        <Checkout />
-        </>
+        <div className="checkout-page">
+            <div className="info-checkout">
+                {console.log("order: ", order)}
+                <h2>TU CARRITO</h2>
+                <div className="container-checkout-products">
+                    {cartProducts.map( (cartProduct) => {
+                        const { title, image, price} = cartProduct
+                        return(
+                            <div className="checkout-item">
+                                <img src={`./assets/${image}`} alt="imagen" />
+                                <div className="info-checkout-item">
+                                    <p>{title}</p>
+                                    <p className="checkout-item-price">$ {price}</p>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+                <div className="info-subtotal">
+                    {cartProducts.map( (cartProduct) => {
+                        const { price } = cartProduct
+                        return(
+                            <>
+                                <button onClick={() => setShowModal(true)}>FINALIZAR PAGO</button>
+                                <div className="total-purchase">
+                                    <h3>RESUMEN DE PEDIDO</h3>
+                                    <div className="item-purchase">
+                                    </div>
+                                    <div className="item-purchase">
+                                        <p>ENTREGA</p>
+                                        <p>GRATIS</p>
+                                    </div>
+                                    <div className="item-purchase">
+                                        <p>TOTAL</p>
+                                        <p>$ {price}</p>
+                                    </div>
+                                </div>
+                            </>
+                        )
+                    })}
+                </div>
+                {showModal && 
+                    <Modal title="DATOS DE CONTACTO" close={() => setShowModal()}>
+                        {success ? (
+                            <>
+                               <h2>Su orden se genero correctamente</h2>
+                               <p>ID de compra : {success}</p>
+                            </>
+                        ) : (
+                            <form onSubmit={submitData}>
+                                <input 
+                                    type='text' 
+                                    name='name' 
+                                    placeholder='Ingrese el nombre'
+                                    onChange={handleChange}
+                                    value={formData.name}
+                                />
+                                <input 
+                                    type='number' 
+                                    name='phone' 
+                                    placeholder='Ingrese el telefono' 
+                                    value={formData.phone}
+                                    onChange={handleChange}
+                                />
+                                <input 
+                                    type='email' 
+                                    name='email' 
+                                    placeholder='Ingrese el mail'
+                                    value={formData.email}
+                                    onChange={handleChange}
+
+                                />
+                                <button type="submit">Enviar</button>
+                            </form>
+                        )}
+                    </Modal>
+                }
+        </div>
     )
 }
 
--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,23 +1,11 @@
-import { CartContext } from "../context/CartContext";
-import { useContext } from "react";
-
+import Cart from '../components/Cart/Cart'
 
 const Checkout = () => {
-  const { cartProducts } = useContext(CartContext);
-  let demo = cartProducts[0];
-  return (
-    <div>
-      <h1>Pagina de Checkout</h1>
-      {demo.map((product) => (
-        <div key={product.id}>
-          <h3>{product.title}</h3>
-          <p>{product.category}</p>
-          <p>{product.price}</p>
+    return(
+        <div className='main-container'>
+            <Cart />
         </div>
-      ))}
-    </div>
-  );
-};
+    )
+}
 
-
-export default Checkout;
+export default Checkout
